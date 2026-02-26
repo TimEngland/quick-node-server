8eae0336a3643e8cba9c8d5555a333c5ab827f01
TimEngland <152618667+TimEngland@users.noreply.github.com>
2026-02-26 19:44:42 +0100

    file2

--- server.js
+++ server.js
@@ -8,6 +8,20 @@ http.createServer((req, res) => {
   // Map URL to file
   if (req.url === '/myfile') {
     const filePath = path.join(__dirname, 'file.txt');
+    
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading file');
+      }
+
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(data);
+    });
+  } else if (req.url === '/myfile2') {
+    const filePath = path.join(__dirname, 'file2.txt');
+    
 
     fs.readFile(filePath, (err, data) => {
       if (err) {
